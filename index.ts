d5854454c35ce4de14885510c81211039a05f256
rom hay laptop <romhayh@gmail.com>
2021-12-02 15:38:41 +0200

    feat(jobs.ts): added get all jobs stub

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Response } from 'express';
+import express from 'express';
 import {logger} from './logger';
 import {router as jobsRouter} from './routes/jobs';
 require('dotenv').config(); 
--- routes/jobs.ts
+++ routes/jobs.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { logger } from '../logger';
+
 
 export const router = express.Router();
 
-router.get("/units/:unitId/jobs", (req, res) => {
+router.get("/units/:unitId", (req, res) => {
+    logger.info(`/jobs/units/${req.params.unitId} GET request`);
     res.send(req.params.unitId);
+    logger.info(`sent a response`);
 });
+
