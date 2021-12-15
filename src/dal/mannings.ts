68c29e268e4dad1c44d2d7bb6a783df5dfb6ec9c
rom hay laptop <romhayh@gmail.com>
2021-12-15 12:57:21 +0200

    feat(dal/mannings.ts): added update manning function

--- src/dal/mannings.ts
+++ src/dal/mannings.ts
@@ -1,11 +1,16 @@
 import fs from 'fs';
+import { isEqual } from 'lodash';
 
 import { logger } from '../logger';
 import { Manning } from '../types/mannings';
 
 const MANNIGS_FILE = 'db/mannings.json';
 
-const mannings : Manning[] = JSON.parse(fs.readFileSync(MANNIGS_FILE, 'utf8'));
+const mannings: Manning[] = JSON.parse(fs.readFileSync(MANNIGS_FILE, 'utf8'));
+
+const updateManningsFile: () => void = () => { 
+    fs.writeFileSync(MANNIGS_FILE, JSON.stringify(mannings), 'utf-8') 
+}
 
 export const getAllMannings: (unitId: number) => Manning[] = (unitId) => {
     logger.info(`dal: get all mannings request of unit ${unitId}`);
@@ -13,10 +18,21 @@ export const getAllMannings: (unitId: number) => Manning[] = (unitId) => {
     return mannings;
 };
 
-export const insertManning : (manning : Manning) => void = (manning) => {
+export const insertManning: (manning: Manning) => void = (manning) => {
     logger.info(`dal: insert a new manning`);
     mannings.push(manning);
-    fs.writeFileSync(MANNIGS_FILE, JSON.stringify(mannings), 'utf-8');
+    updateManningsFile();
 }
 
-export default { getAllMannings };
+export const updateManning: (manning: Manning) => void = (manning) => {
+    logger.info(`dal: update an existing manning`);
+
+    const updatedManningIndex = mannings.findIndex(
+        currentManning => currentManning.manningId === manning.manningId
+    );
+
+    mannings[updatedManningIndex] = manning;
+    updateManningsFile();
+}
+
+export default { getAllMannings, insertManning, updateManning };
